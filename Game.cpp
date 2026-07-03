daff795477f729da1e35a22c7a3e0b35499823b9
ElijahBThomas <EBthomas@student.fullsail.edu>
2026-07-03 02:14:03 -0700

    Revert "Feature/todos"

--- Game.cpp
+++ Game.cpp
@@ -6,21 +6,6 @@ Game::Game()
 	Reset();
 }
 
-Game::~Game() 
-{
-	ClearBricks();
-}
-
-void Game::ClearBricks()
-{
-	for (Box* brick : bricks)
-	{
-		delete brick;
-	}
-
-	bricks.clear();
-}
-
 void Game::Reset()
 {
 	Console::SetWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
@@ -30,26 +15,17 @@ void Game::Reset()
 	paddle.x_position = 32;
 	paddle.y_position = 30;
 
-	ball.visage = 'o';
+	ball.visage = 'O';
 	ball.color = ConsoleColor::Cyan;
 	ResetBall();
 
 	// TODO #2 - Add this brick and 4 more bricks to the vector
-	ClearBricks();
-	for (int i = 0; i < brick_amount; i++)
-	{
-		Box brick;
-		brick.width = 10;
-		brick.height = 2;
-		brick.x_position = 0 + (i * 10);
-		brick.y_position = 5;
-		brick.doubleThick = true;
-		brick.color = ConsoleColor::DarkCyan;
-
-		Box* brick_ptr = new Box(brick);
-		bricks.push_back(brick_ptr);
-	}
-	
+	brick.width = 10;
+	brick.height = 2;
+	brick.x_position = 0;
+	brick.y_position = 5;
+	brick.doubleThick = true;
+	brick.color = ConsoleColor::DarkGreen;
 }
 
 void Game::ResetBall()
@@ -86,36 +62,14 @@ bool Game::Update()
 //  All rendering, including text, should occur in the Render function
 void Game::Render() const
 {
-	bool pause = false;
 	Console::Lock(true);
 	Console::Clear();
 	
-	if (bricks.size() == 0)
-	{
-		printf("You win! Press R to play again.");
-		pause = true;
-	}
+	paddle.Draw();
+	ball.Draw();
 
-	if (ball.y_position > 32)
-	{
-		printf("You lose. Press R to play again.");
-		pause = true;
-	}
-
-	if (!pause)
-	{
-		paddle.Draw();
-		ball.Draw();
-
-		// TODO #3 - Update render to render all bricks
-		if (bricks.size() > 0)
-		{
-			for (Box* brick : bricks)
-			{
-				brick->Draw();
-			}
-		}
-	}
+	// TODO #3 - Update render to render all bricks
+	brick.Draw();
 
 	Console::Lock(false);
 }
@@ -123,30 +77,17 @@ void Game::Render() const
 void Game::CheckCollision()
 {
 	// TODO #4 - Update collision to check all bricks
-	for (int i = 0; i < bricks.size(); i++)
+	if (brick.Contains(ball.x_position + ball.x_velocity, ball.y_position + ball.y_velocity))
 	{
-		Box* brick = bricks[i];
+		brick.color = ConsoleColor(brick.color - 1);
+		ball.y_velocity *= -1;
 
-		if (brick->Contains(ball.x_position + ball.x_velocity, ball.y_position + ball.y_velocity))
-		{
-			brick->color = ConsoleColor(brick->color - 1);
-			ball.y_velocity *= -1;
+		// TODO #5 - If the ball hits the same brick 3 times (color == black), remove it from the vector
 
-			// TODO #5 - If the ball hits the same brick 3 times (color == black), remove it from the vector
-			if (brick->color == 0)
-			{
-				delete brick;
-				bricks.erase(bricks.begin() + i);
-			}
-		}
 	}
-	
 
 	// TODO #6 - If no bricks remain, pause ball and display (render) victory text with R to reset
-	if (bricks.size() == 0)
-	{
-		ball.moving = false;
-	}
+
 
 	if (paddle.Contains(ball.x_position + ball.x_velocity, ball.y_velocity + ball.y_position))
 	{
@@ -154,8 +95,4 @@ void Game::CheckCollision()
 	}
 
 	// TODO #7 - If ball touches bottom of window, pause ball and display (render) defeat text with R to reset
-	if (ball.y_position > 32)
-	{
-		ball.moving = false;
-	}
 }
--- Game.h
+++ Game.h
@@ -1,7 +1,6 @@
 #pragma once
 #include "Box.h"
 #include "Ball.h"
-#include <vector>
 
 class Game
 {
@@ -9,16 +8,12 @@ class Game
 	Box paddle;
 
 	// TODO #1 - Instead of storing 1 brick, store a vector of bricks (by value)
-	int brick_amount = 5;
-
-	std::vector<Box*> bricks = {};
+	Box brick;
 
 public:
 	Game();
-	~Game();
 	bool Update();
 	void Render() const;
-	void ClearBricks();
 	void Reset();
 	void ResetBall();
 	void CheckCollision();
