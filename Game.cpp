415e0e23fe7c23d69d9db3a0a6c5250eb273f6ab
Elijah <EBThomas@student.fullsail.edu>
2026-07-03 00:12:41 -0700

    2. Edit Game.h to #include <vector> and replace the single brick for a vector of bricks.

--- Game.cpp
+++ Game.cpp
@@ -20,12 +20,16 @@ void Game::Reset()
 	ResetBall();
 
 	// TODO #2 - Add this brick and 4 more bricks to the vector
-	brick.width = 10;
-	brick.height = 2;
-	brick.x_position = 0;
-	brick.y_position = 5;
-	brick.doubleThick = true;
-	brick.color = ConsoleColor::DarkGreen;
+
+	for (int i = 0; i < bricks.size(); i++)
+	{
+		bricks[i].width = 10;
+		bricks[i].height = 2;
+		bricks[i].x_position = 0 + (i * 10);
+		bricks[i].y_position = 5;
+		bricks[i].doubleThick = true;
+		bricks[i].color = ConsoleColor::DarkGreen;
+	}
 }
 
 void Game::ResetBall()
--- Game.h
+++ Game.h
@@ -9,7 +9,8 @@ class Game
 	Box paddle;
 
 	// TODO #1 - Instead of storing 1 brick, store a vector of bricks (by value)
-	std::vector<Box> bricks;
+	Box brick, brick1, brick2, brick3, brick4;
+	std::vector<Box> bricks = { brick, brick1, brick2, brick3, brick4 };
 
 public:
 	Game();
